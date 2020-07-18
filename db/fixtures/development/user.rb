9e15095892360a2a26e090d2f2cea49336e0b48d
Ranbir Singh <ransingh57@yahoo.com>
2020-07-19 03:13:05 +1000

    added fixtures

---
+++ db/fixtures/development/user.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+User.seed(:id,
+  { id: 1, first_name: 'Ranbir', last_name: 'Singh', email: 'ranbir@gmail.com' }
+)
---
+++ db/fixtures/recurrences.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+Recurrence.seed(:id,
+  { id: 1, name: 'weekly', sequence: 1 }
+)
+Recurrence.seed(:id,
+  { id: 2, name: 'monthly', sequence: 2 }
+)
+Recurrence.seed(:id,
+  { id: 3, name: 'quaterly', sequence: 3 }
+)
+Recurrence.seed(:id,
+  { id: 4, name: 'half-yearly', sequence: 4 }
+)
+Recurrence.seed(:id,
+  { id: 5, name: 'yearly', sequence: 5 }
+)
